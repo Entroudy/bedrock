939d67e4210b393cb296da0dd69120fd6c7dd7e9
Entroudy <50957792+Entroudy@users.noreply.github.com>
2021-11-30 16:06:12 +0100

    Add files via upload
    11.30 melós push

---
+++ bedrock/products.sql
@@ -0,0 +1,305 @@
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('A La Maison','ANA Holding Kft.','Thursday','',
+'daniel@bedrock.farm','2','25331549-2-41','david.csato8823@gmail.com',
+'Csató Dávid','36704173096','Készpénz','490');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Alessio','Alessio`2009 Kft.','Thursday','',
+'daniel@bedrock.farm','2','14960828-2-41','schmidtjozsef83@gmail.com',
+'Schmidt József','36203181218','Készpénz','490');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Anton Cafe','Wholesome Holistic Kft.','','',
+'daniel@bedrock.farm','','26663944-2-43','',
+'','6666','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Arch&Beans','Arch&Beans Kft.','','',
+'daniel@bedrock.farm','3','24179687-2-42','kovacs88tamas@gmail.com',
+'Kovács Tamás','36706314444','Banki utalás 8 napon belül','290');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Ázsia Bt 4 égtáj izei','ÁZSIA BT.','Thursday','',
+'daniel@bedrock.farm','2','28089595-2-43','pakuildikoo@gmail.com',
+'Paku Ildikó','36301518131','Banki utalás 8 napon belül','0');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Bageri','Wholesome Holistic Kft.','','',
+'daniel@bedrock.farm','','26663944-2-43','',
+'','6666','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Bearkery','Bearkery Kft.','Monday','',
+'daniel@bedrock.farm','2','28786704-2-13','info@bearkery.hu',
+'Medvéné Ludas Mónika','36204007041','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Benedict Cloud9','Janovszky Gasztro Kft','Thursday','',
+'daniel@bedrock.farm','3','25799006-2-41','uzletvezetocloud9@gmail.com',
+'Tóth Edit','36302575656','Banki utalás 8 napon belül','490');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Café 57','GLOBO FOOD Kft.','Friday','',
+'daniel@bedrock.farm','3','24840013-2-42','',
+'Péli Dániel','36203488840','Banki utalás 8 napon belül','490');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Café Maran','GAMI-GRAND Kft.','','',
+'daniel@bedrock.farm','1','11574271-2-03','kumigergo955@gmail.com',
+'Kumi Gergő','36307804802','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Cafe Tripoli','Tripolisz Kft.','','',
+'daniel@bedrock.farm','','26545697-2-43','',
+'','6666','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Culinaris','Culinaris Kft.','Tuesday','',
+'daniel@bedrock.farm','1','13039831-2-41','buy@culinaris.hu',
+'Strommer Adél','6666','Banki utalás 8 napon belül','490');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Essence Delicates','ESSENCE MAGYARORSZÁG Kft','Tuesday','',
+'daniel@bedrock.farm','2','26507978-2-41','golyasborhaz@gmail.com',
+'Tibor Kálmán','36706297868','Készpénz','690');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Essência','Essenciagourmet Kft.','','',
+'daniel@bedrock.farm','1','27284137-2-43','',
+'Tiago Sabarigo','6666','Banki utalás 8 napon belül','0');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Fausto`s','CARNIA Kft.','','',
+'daniel@bedrock.farm','','10873436-2-42','',
+'','6666','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Félix','Vízház Gasztronómia Zrt.','Tuesday','',
+'daniel@bedrock.farm','1','26100478-2-41','jeremycayron51@hotmail.com',
+'Jeremy Cayron','36709464006','Banki utalás 8 napon belül','290');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Food Revolution','FOOD REVOLUTION Kft.','','',
+'daniel@bedrock.farm','','29173758-2-05','gabooorr@gmail.com',
+'Gáspár Gábor','36304925924','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Four Seasons','Four Seasons Budapest Kft.','','',
+'daniel@bedrock.farm','','11946779-2-41','arpad.gyorffy@fourseasons.com',
+'Győrffy Árpád','36308244642','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('France Gourmet','France Gourmet Foods Kft.','Thursday','',
+'daniel@bedrock.farm','1','24978280-2-41','jloczki@francegourmet.hu',
+'Lóczki János','6666','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Füge','FÜGE BOLT kft.','','Szállítólevél',
+'daniel@bedrock.farm','','13955096-2-41','',
+'Lipthay-Nagy Tamás','36205559599','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Gentry Coffee&Brunch','Valentin Ádám EV.','Tuesday','',
+'daniel@bedrock.farm','2','69545621-2-41','',
+'Valentin Ádám','36204872262','Készpénz','0');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Héjakút Drogéria','Farkas Zsuzsanna EV.','','',
+'daniel@bedrock.farm','','67481943-2-23','',
+'','6666','Készpénz','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Honey Brunch','ROOM 4 COFFEE Kft.','Tuesday','',
+'daniel@bedrock.farm','3','28811954-2-41','',
+'','6666','Készpénz','0');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Hosty Totsy','Bar and Barber Kft.','Thursday','',
+'daniel@bedrock.farm','2','25752852-2-13','pakaylili.lfgeb@gmail.com',
+'Rikk-Pákay Lili','36707704177','Banki utalás 15 napon belül','290');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Indian Palate','Royal Heritage Kft.','Friday','',
+'daniel@bedrock.farm','2','26525376-2-41','manager@indianpalate.hu',
+'Kondacs Valéria','6666','Banki utalás 8 napon belül','0');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('LEO','Leo Restaurants Kft.','','',
+'daniel@bedrock.farm','1','23055348-2-41','',
+'Varga Roland','36306648013','Banki utalás 8 napon belül','290');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('London Coffee Society','THREE WAVE KFT.','','',
+'daniel@bedrock.farm','3','25178294-2-41','',
+'Tünde','36305670044','Banki utalás 8 napon belül','290');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Matild Palace','Melis Operation Kft.','','',
+'daniel@bedrock.farm','1','17783147-5-41','',
+'Fehér Attila','36704160447','Banki utalás 8 napon belül','290');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Mazel Tov','Mazel Tov House Kft.','Friday','',
+'daniel@bedrock.farm','1','24827555-2-42','',
+'Deák Attila','36203300779','Banki utalás 8 napon belül','290');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Mimo','OPULENTIA-sztv kft','Thursday','',
+'daniel@bedrock.farm','1','25905507-2-13','juhaszaron99@gmail.com',
+'Juhász Áron','36209224441','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Multibridge','Multibridge Kft.','Friday','',
+'daniel@bedrock.farm','3','10377961-2-41','',
+'','6666','Készpénz','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('NormaGrand','NormaBro Kft.','Thursday','',
+'daniel@bedrock.farm','1','23802786-2-43','kkrisztiankorodi@gmail.com',
+'Kóródi Krisztián','36703135274','Banki utalás 8 napon belül','490');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Ramazuri','GP Gastro Kft.','','',
+'daniel@bedrock.farm','1','25276615-2-41','liebl.martin7@gmail.com',
+'Liebl Martin','36305287455','Banki utalás 8 napon belül','490');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Rusty','URBAN FAMILY Zrt.','Thursday','',
+'daniel@bedrock.farm','2','25524482-2-19','',
+'Hortobágyi Dalma','36202653765','Készpénz','490');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Rutin Étterem','Rutindeli Kft.','','',
+'daniel@bedrock.farm','','28749213-2-13','info@rutinbudaors.hu',
+'Pallag Dávid','36306553272','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('SushiGo','SushiGo! Vendéglátó és Szolgáltató Kft.','Friday','',
+'daniel@bedrock.farm','3','12612677-2-03','csaba.a.denes@gmail.com',
+'Dénes Csaba','6666','Készpénz','0');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Tereza','LA FUENTE Kft.','Friday','',
+'daniel@bedrock.farm','1','13463830-2-42','mate.gaal@outlook.hu',
+'Gaál Máté','36203750911','Banki utalás 8 napon belül','290');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Törökméz','MDR CAFE Kft.','Friday','',
+'daniel@bedrock.farm','2','24702713-2-41','zoltan.csonka@torokmez.hu',
+'Csonka Zoltán','36703646907','Készpénz','490');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Trattoria Cardinale','Cardinale BDPST Kft.','Friday','',
+'daniel@bedrock.farm','2','27427525-2-42','andras.ruppl@gmail.com',
+'Ruppl András','36702876735','Banki utalás 8 napon belül','0');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Tuktuk','HOTEL PEST KFT.','Tuesday','Szállítólevél',
+'daniel@bedrock.farm','2','12090204-2-42','',
+'Fuchs Dávid','36202301704','Banki utalás 8 napon belül','290');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Véndiák','VÉNDIÁK ÉTTEREM Kft.','','',
+'daniel@bedrock.farm','','22672373-2-41','',
+'Ignác Attila','36702447974','Banki utalás 8 napon belül','');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Wokzilla','two pipi Kft','Tuesday','',
+'daniel@bedrock.farm','3','25419292-2-42','mark@wokzilla.hu',
+'Környei Márk','36703178888','Készpénz','0');
+
+
+INSERT INTO Products( 'Name', 'CompanyName', 'DeliveryTime', 'Comment', 'AccountManager', 'Priority', 'TaxNumber', 
+'ContactPersonEmail', 'ContactPersonName', 'ContactPersonPhone', 'PaymentMethod', 'DeliveryFee') 
+VALUES ('Zerge','Benedek Tervező Iroda Kft','Friday','',
+'daniel@bedrock.farm','3','13197177-2-43','',
+'Zatykó András','36305030340','Készpénz','490');
+
+
+
+
